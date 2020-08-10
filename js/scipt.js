7483ee67db106a032d8a52f476ca86de5ba79c51
Forrest Reiland <you@example.com>
2020-08-10 10:56:20 -0700

    add subtract, mult, division fucntion to script.js
    Co-authored-by: Janet Karpenske <janetkarpenske@gmail.com>
    Co-authored-by: Ben Stoller <stollersystemsound@gmail.com>

--- js/scipt.js
+++ js/scipt.js
@@ -1,7 +1,22 @@
-function add(number1, number2){
+function add(number1, number2) {
   return number1 + number2;
-  }
-  const number1 = parseInt(promt("Enter a number:"));
-  const number2 = parseInt(promt('Enter another number:'));
-  alert(add(10,5));
+}
+
+function subtract(number1, number2){
+  return number1 - number2;
+}
+
+function multiply(number1, number2){
+  return number1 * number2;
+}
   
+function divide (number1, number2) {
+  return number1 / number2;
+}
+
+
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
+const result = subtract(number1, number2);
+alert(result);
+
