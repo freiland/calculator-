fc0b03014db4c37d8c39be65e4c9820fedd34cc5
Forrest Reiland <you@example.com>
2020-08-12 13:56:00 -0700

    add second subtraction feature to calculator

--- js/scipt.js
+++ js/scipt.js
@@ -1,3 +1,5 @@
+// Business Logic 
+
 const add =function(number1, number2) {
   return number1 + number2;
 }
@@ -14,12 +16,33 @@ const divide = function(number1, number2) {
   return number1 / number2;
 }
 
+// User interface logic
+
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
 
   const number1 = parseInt($("#add1").val());
   const number2 = parseInt($("#add2").val());
-  alert(add(number1, number2));
+  const result = add(number1, number2);
+  $("#output").text(result);
+  });
+
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
+  const result = subtract(number1, number2);
+  $("#output").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
+  const result = multiply(number1, number2);
+  $("#output").text(result);
   });
 });
