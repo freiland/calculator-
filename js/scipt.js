8a13310000feb49fe8c258931200f33fd459f697
Forrest Reiland <you@example.com>
2020-08-12 13:23:56 -0700

    add preventDefault method and .submit method to scripts

--- js/scipt.js
+++ js/scipt.js
@@ -1,22 +1,25 @@
-function add(number1, number2) {
+const add =function(number1, number2) {
   return number1 + number2;
 }
 
-function subtract(number1, number2){
+const subtract = function(number1, number2){
   return number1 - number2;
 }
 
-function multiply(number1, number2){
+const multiply = function(number1, number2){
   return number1 * number2;
 }
   
-function divide (number1, number2) {
+const divide = function(number1, number2) {
   return number1 / number2;
 }
 
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-const result = subtract(number1, number2);
-alert(result);
-
+  const number1 = parseInt($("#add1").val());
+  const number2 = parseInt($("#add2").val());
+  alert(add(number1, number2));
+  });
+});
